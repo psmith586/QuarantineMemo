232fbe64688ee8b94119b8cceaa29584bb853a4b
Phillip Smith <psmith@Phillips-MacBook-Pro.local>
2020-04-11 19:58:46 -0700

    refactor/revert to skeleton

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,56 +1,13 @@
 /* @flow */
-import React, {Component} from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
 
-//redux
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import {updateEmail, updatePassword, signup} from './actions/User';
-import {TextInput} from 'react-native-gesture-handler';
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({updateEmail, updatePassword, signup}, dispatch);
-};
-
-const mapStateToProps = state => {
-  return {
-    user: state.user,
-  };
-};
-
-class SignUp extends Component {
-  handleSignUp = () => {
-    this.props.signup();
-    this.props.navigation.navigate('Home');
-  };
-
+export default class SignUp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput
-          value={this.props.user.email}
-          onChangeText={email => this.props.updateEmail(email)}
-          placeholder="Email"
-        />
-        <TextInput
-          value={this.props.user.password}
-          onChangeText={password => this.props.updatePassword(password)}
-          placeholder="Password"
-          secureTextEntry={true}
-        />
-        <Button title="Sign Up" onPress={this.handleSignUp} />
+      <View>
+        <Text> Sign up page </Text>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(SignUp);
--- components/login.js
+++ components/login.js
@@ -1,77 +1,12 @@
-/* @flow */
-import React, {Component} from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
 
-import Logo from './Logo';
-import {TextInput} from 'react-native-gesture-handler';
-
-//redux
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import {updateEmail, updatePassword, login} from './actions/User';
-
-//firebase
-import auth from '@react-native-firebase/auth';
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({updateEmail, updatePassword, login});
-};
-
-const mapStateToProps = state => {
-  return {
-    user: state.user,
-  };
-};
-
-class Login extends Component {
-  handleLogin = () => {
-    const {email, password} = this.state;
-
-    auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(() => this.props.navigation.navigate('Home'))
-      .catch(error => console.log(error));
-  };
-
+export default class Login extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.logo}>
-          <Logo />
-        </View>
-        <View style={styles.loginForm}>
-          <TextInput
-            value={this.props.user.email}
-            onChangeText={email => this.props.updateEmail(email)}
-            placeholder="Email"
-          />
-          <TextInput
-            value={this.props.user.password}
-            onChangeText={password => this.props.updatePassword(password)}
-            placeholder="Password"
-            secureTextEntry={true}
-          />
-        </View>
-        <Button title="Login" onPress={this.handleLogin} />
-        <Button title="Sign Up" onPress={this.navigation.navigate('SignUp')} />
+      <View>
+        <Text> Login Page </Text>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  logo: {
-    alignSelf: 'center',
-  },
-  loginForm: {
-    alignSelf: 'auto',
-  },
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Login);
