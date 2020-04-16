d5436808c5b6b292ac6201f5fe49fba6b0cd9883
Phillip Smith <psmith@Phillips-MacBook-Pro.local>
2020-04-16 16:12:58 -0700

    package updates

--- components/App.js
+++ components/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Navigator from './Navigator'
 import '@react-native-firebase/auth'
+import '@react-native-firebase/firestore'
 
 const App = () => (
   <Navigator />
