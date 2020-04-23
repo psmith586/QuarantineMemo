a66aa3da76d79dc2a3b4f6f8f8e1169ea5f6d7d2
Phillip Smith <psmith@Phillips-MacBook-Pro.local>
2020-04-23 10:34:22 -0700

    checkbox form

--- components/CreateMemo.js
+++ components/CreateMemo.js
@@ -1,7 +1,17 @@
 import React, { memo } from 'react'
 import { View } from 'react-native'
+import { Checkbox } from '@react-native-community/checkbox'
 
 const CreateMemo = ({ navigation }) => {
+
+  return(
+    <View>
+      <Checkbox />
+      <Checkbox />
+      <Checkbox />
+      <Checkbox />
+    </View>
+  );
 
 };
 
